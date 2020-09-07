89e8867d08bf6d8d2af60ab861d5e5d9d757ddf7
haidarrifki <19185336+haidarrifki@users.noreply.github.com>
2020-09-07 13:41:58 +0700

    modify font css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    // Use *.tsx if using TypeScript
-    './pages/**/*.jsx',
-    './components/**/*.jsx'
-  ],
+  // purge: [
+  //   // Use *.tsx if using TypeScript
+  //   './pages/**/*.jsx',
+  //   './components/**/*.jsx'
+  // ],
+  purge: ['./components/*.{js,ts,jsx,tsx}', './pages/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
